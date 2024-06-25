a306195f4f38eb7ef97318be60a83dd9a89458c0
Muhammad Naufal Kurniawan <naufalkurniawan605@gmail.com>
2024-06-25 09:23:30 +0700

    implement file upload on API m_user

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -27,10 +28,16 @@ class UserModel extends Authenticatable implements JWTSubject
         'username',
         'password',
         'nama',
+        'image'
     ];
 
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    protected function image() : Attribute {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image)
+        );
     }
 }
