a575e90c1f5fd07046b8c538e7bc0cc7456af06e
Muhammad Naufal Kurniawan <naufalkurniawan605@gmail.com>
2024-06-25 10:39:22 +0700

    implement file upload on API m_barang

--- app/Http/Controllers/Api/BarangController.php
+++ app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -14,8 +15,22 @@ class BarangController extends Controller
     }
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            "kategori_id" => 'required',
+            "barang_kode" => 'required',
+            "barang_nama" => 'required',
+            "harga_beli" => 'required',
+            "harga_jual" => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
         return response()->json(
-            BarangModel::create($request->all()),
+            BarangModel::create(array_merge($request->except('image'),[
+                'image' => $request->image->hashName()
+            ])),
             201
         );
     }
